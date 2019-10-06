2640fd341b76418362fed0138dcad02220e0fbb1
danRolon <daniel.rolon@yahoo.com>
2019-10-05 22:14:06 -0500

    added styling

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Skills from './components/skills';
 
 function App() {
   return (
-    <div className="App">
+    <div id='top' className="App">
       <header>
         <NavBar/>
       </header>
